779b3e254c16be6268147f7524b6c8a2a9192897
Teerth-the-coder <154464588+Teerth-the-coder@users.noreply.github.com>
2024-04-01 18:23:12 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-player1Name = localStorage.getItem("username");
-player2Name = localStorage.getItem("username2");
+player1Name = localStorage.getItem("player1Name");
+player2Name = localStorage.getItem("player2Name");
 
 player1Score = 0;
 player2Score = 0;
@@ -25,6 +25,33 @@ function send() {
     document.getElementById("output").style.display = "block";
     document.getElementById("question").innerHTML = num1 + "X" + num2;
 
-     document.getElementById("number1").value = "";
-     document.getElementById("number2").value = "";
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+}
+
+function check() {
+    get_answer = document.getElementById("answerBox").value;
+    if (get_answer == ans) {
+        if (qt == "P1") {
+            player1Score += 1;
+            document.getElementById("playerOneName").innerHTML = player1Name + ": &nbsp;" + player1Score;
+        } else {
+            player2Score += 1;
+            document.getElementById("playerTwoName").innerHTML = player2Name + ": &nbsp;" + player2Score;
+        }
+    }
+
+    if (qt == "P1") {
+        qt = "P2";
+        at = "P1";
+        document.getElementById("questionTurn").innerHTML = "Question Turn - " + player2Name;
+        document.getElementById("answerTurn").innerHTML = "Answer Turn - " + player1Name;
+    } else {
+        qt = "P1";
+        at = "P2";
+        document.getElementById("questionTurn").innerHTML = "Question Turn - " + player1Name;
+        document.getElementById("answerTurn").innerHTML = "Answer Turn - " + player2Name;
+    }
+
+    document.getElementById("output").style.display = "none";
 }
